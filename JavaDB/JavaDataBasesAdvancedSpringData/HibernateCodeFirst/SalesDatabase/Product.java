cf4a331e2fe9a9487aa3c77ae2bc350c3e2a183b
OlegMirchev <olegmirchev@gmail.com>
2022-10-20 16:17:09 +0300

    Added homework

---
+++ JavaDB/JavaDataBasesAdvancedSpringData/HibernateCodeFirst/SalesDatabase/Product.java
@@ -0,0 +1,76 @@
+package HibernateCodeFirst.SalesDatabase;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double quantity;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @OneToMany(mappedBy = "product", targetEntity = Sale.class)
+    private Set<Sale> sales;
+
+    public Product() {
+
+    }
+
+    public Product(int id, String name, double quantity, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Set<Sale> getSales() {
+        return sales;
+    }
+
+    public void setSales(Set<Sale> sales) {
+        this.sales = sales;
+    }
+}
---
+++ JavaDB/JavaDataBasesAdvancedSpringData/HibernateCodeFirst/SalesDatabase/Sale.java
@@ -0,0 +1,78 @@
+package HibernateCodeFirst.SalesDatabase;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "sale")
+public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    private Customer customer;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "store_location_id", referencedColumnName = "id")
+    private StoreLocation storeLocation;
+
+    private LocalDate date;
+
+    public Sale() {
+
+    }
+
+    public Sale(int id, Product product, Customer customer, StoreLocation storeLocation) {
+        this.id = id;
+        this.product = product;
+        this.customer = customer;
+        this.storeLocation = storeLocation;
+        this.date = LocalDate.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public StoreLocation getStoreLocation() {
+        return storeLocation;
+    }
+
+    public void setStoreLocation(StoreLocation storeLocation) {
+        this.storeLocation = storeLocation;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
